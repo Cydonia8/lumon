e6e4d8c895abf92c0ad6981f290e1af8d96d4baf
Álvaro Blanco Lucena <36717062+Cydonia8@users.noreply.github.com>
2023-01-14 13:36:29 +0100

    menu responsive

--- script/script_menu.js
+++ script/script_menu.js
@@ -6,7 +6,9 @@ const menu = document.querySelector("header")
 abrir_menu.addEventListener("click", ()=>{
     if(menu.classList.contains("mostrar-menu")){
         menu.classList.remove("mostrar-menu")
+        abrir_menu.classList.remove("fijar-mostrar-menu")
     }else{
         menu.classList.add("mostrar-menu")
+        abrir_menu.classList.add("fijar-mostrar-menu")
     }
 })
